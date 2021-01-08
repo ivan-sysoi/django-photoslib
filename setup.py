b4cbdce25974418708df933cbc6d7168d548eb8b
Ivan Sysoi <ivan.sysoi@gmail.com>
2021-01-08 11:58:17 +0300

    Increment version to 0.0.8.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name="django-photoslib",
-    version='0.0.7',
+    version='0.0.8',
     description=read('DESCRIPTION'),
     long_description=read('README.md'),
     license='MIT',
